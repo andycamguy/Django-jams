e400778cf83d9b341cc7dc191603226a0d29eb94
Andrew Ross <aross9913@gmail.com>
2023-08-02 22:21:10 +0000

    had to redo some stuff

--- djangojams/djangojams/asgi.py
+++ djangojams/djangojams/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for kickjams project.
+ASGI config for djangojams project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'kickjams.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djangojams.settings')
 
 application = get_asgi_application()
--- djangojams/djangojams/wsgi.py
+++ djangojams/djangojams/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for kickjams project.
+WSGI config for djangojams project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'kickjams.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djangojams.settings')
 
 application = get_wsgi_application()
